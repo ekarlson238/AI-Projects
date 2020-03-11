cb89b975c7efc90a5efc3a2c34a90990131ea719
ekarlson238 <ekarlson238@gmail.com>
2020-03-11 08:17:07 -0500

    Ch6

---
+++ Assets/Scripts/Ch6/VehicleAvoidance.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+public class VehicleAvoidance : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float mass = 5.0f;
+    public float force = 50.0f;
+    public float minimumDistToAvoid = 20.0f;
+    private float curSpeed;
+    private Vector3 targetPoint;
+
+    void Start()
+    {
+        mass = 5.0f;
+        targetPoint = Vector3.zero;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Click anywhere to move the vehicle.");
+    }
+
+    //Update is called once per frame
+    void Update() 
+    {
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay
+        (Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) &&
+        Physics.Raycast(ray, out hit, 100.0f))
+        {
+            targetPoint = hit.point;
+        }
+        Vector3 dir = (targetPoint - transform.position);
+        dir.Normalize();
+        AvoidObstacles(ref dir);
+
+
+
+        if (Vector3.Distance(targetPoint, transform.position) < 3.0f) return;
+
+        curSpeed = speed * Time.deltaTime;
+
+        var rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp
+        (transform.rotation, rot, 5.0f *
+        Time.deltaTime);
+        transform.position += transform.forward *
+         curSpeed;
+    }
+
+    public void AvoidObstacles(ref Vector3 dir)
+    {
+        RaycastHit hit;
+
+        int layerMask = 1 << 8;
+
+        if (Physics.Raycast(transform.position,
+        transform.forward, out hit,
+        minimumDistToAvoid, layerMask))
+        {
+            Vector3 hitNormal = hit.normal;
+            hitNormal.y = 0.0f; 
+            dir = transform.forward + hitNormal * force;
+        }
+    }
+}
---
+++ Assets/Scripts/Ch6/VehicleFollowing.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+public class VehicleFollowing : MonoBehaviour
+{
+    public Path path;
+    public float speed = 20.0f;
+    public float mass = 5.0f;
+    public bool isLooping = true;
+
+    private float curSpeed;
+    private int curPathIndex;
+    private float pathLength;
+    private Vector3 targetPoint;
+    Vector3 velocity;
+
+    void Start()
+    {
+        pathLength = path.Length;
+        curPathIndex = 0;
+
+        velocity = transform.forward;
+    }
+
+    void Update()
+    {
+        curSpeed = speed * Time.deltaTime;
+        targetPoint = path.GetPoint(curPathIndex);
+
+        if (Vector3.Distance(transform.position, targetPoint) <
+        path.Radius)
+        {
+
+            if (curPathIndex < pathLength - 1) curPathIndex++;
+            else if (isLooping) curPathIndex = 0;
+            else return;
+        }
+
+        if (curPathIndex >= pathLength) return;
+
+        if (curPathIndex >= pathLength - 1 && !isLooping)
+            velocity += Steer(targetPoint, true);
+        else velocity += Steer(targetPoint);
+
+        transform.position += velocity;
+
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+
+    public Vector3 Steer(Vector3 target,
+    bool bFinalPoint = false)
+    {
+        Vector3 desiredVelocity = (target - transform.position);
+        float dist = desiredVelocity.magnitude;
+
+        desiredVelocity.Normalize();
+
+        if (bFinalPoint && dist < 10.0f) desiredVelocity *=
+            (curSpeed * (dist / 10.0f));
+        else desiredVelocity *= curSpeed;
+
+        Vector3 steeringForce = desiredVelocity - velocity;
+        Vector3 acceleration = steeringForce / mass;
+        return acceleration;
+    }
+}
